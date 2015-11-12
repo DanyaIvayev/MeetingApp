51e5f06d90be2dd01248cd63f815c9484f263306
DanyaIvayev <DanyaIvayev@yandex.ru>
2015-11-13 00:05:07 +0400

    Create Dialogs, method for displaying menu and add icon to project

--- app/src/main/java/com/example/meetingapp/MainActivity.java
+++ app/src/main/java/com/example/meetingapp/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.meetingapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.view.ViewConfiguration;
+import java.lang.reflect.Field;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +20,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar ab =getSupportActionBar();
+        ab.setDisplayShowHomeEnabled(true);
+        ab.setIcon(R.drawable.ic_launcher);
+        getOverflowMenu();
     }
 
     @Override
@@ -26,12 +39,76 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        switch(id){
+            case R.id.action_about:{
+                showAboutDialog();
+            } break;
+            case R.id.action_exit:{
+                showAlert();
+            } break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
         return super.onOptionsItemSelected(item);
+    }
+    public void showAlert() {
+
+        new AlertDialog.Builder(this).setIconAttribute(android.R.attr.alertDialogIcon)
+                .setTitle(R.string.quit)
+                .setMessage(R.string.really_quit)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //Stop the activity
+                        finish();
+                    }
+
+                })
+                .setNegativeButton(R.string.no, null)
+                .show();
+    }
+    public void showAboutDialog(){
+        Drawable icon = ContextCompat.getDrawable(getApplicationContext(), android.R.drawable.ic_dialog_info).mutate();
+        icon.setColorFilter(new ColorMatrixColorFilter(new float[] {
+                0.5f,0,0, 0, 0,
+                0, 0.5f, 0, 0, 0,
+                0, 0, 0, 0.5f, 0,
+                0, 0, 0, 1, 0,
+        }));
+        new AlertDialog.Builder(this)
+                .setCancelable(false)
+
+                .setIcon(icon)
+                .setTitle(R.string.about)
+                .setMessage(R.string.aboutText)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+
+                }).show();
+    }
+
+    private void getOverflowMenu() {
+
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- rest/src/main/java/com/example/rest/MainPageSvc.java
+++ rest/src/main/java/com/example/rest/MainPageSvc.java
@@ -54,4 +54,5 @@ public class MainPageSvc {
         return data;
     }
 
+
 }
